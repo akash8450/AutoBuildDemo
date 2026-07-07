4397cdcad0faffa0c312c5b56be6afd5cb5cb4bf
Akash Singh <akash.plabs@gmail.com>
2026-07-07 12:49:22 +0530

    Update Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,65 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+import { logoAndroid, cloudDone } from "ionicons/icons";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>Auto Build Demo</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+      <IonContent className="ion-padding">
+
+        <IonCard>
+          <IonCardContent style={{ textAlign: "center" }}>
+            <IonIcon
+              icon={logoAndroid}
+              style={{
+                fontSize: "80px",
+                color: "#3DDC84",
+              }}
+            />
+
+            <h1>🚀 GitHub Actions APK Build</h1>
+
+            <p>
+              This APK was generated automatically after pushing code to GitHub.
+            </p>
+
+            <IonButton expand="block">
+              Build Successful
+            </IonButton>
+
+            <br />
+
+            <IonButton
+              expand="block"
+              color="success"
+            >
+              <IonIcon icon={cloudDone} slot="start" />
+              Downloaded from GitHub Actions
+            </IonButton>
+
+            <h3 style={{ marginTop: "20px" }}>
+              Version 1.0.1
+            </h3>
+
+          </IonCardContent>
+        </IonCard>
+
       </IonContent>
     </IonPage>
   );
